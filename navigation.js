3547ed9aad89b900f71456f279a01b6ca35f3c50
soccerJoshNumberNine <blue@smittycraft.com>
2017-07-06 21:27:47 -0700

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,39 +1,38 @@
-// Created by footykeeper Copyright (c) 2017
-// View orginal source code: https://github.com/footykeeper/crispy-goggles
-// View website: http://footykeeper.com
-// This is navigation.js. This Javascript is designed to fill in the navigation links on every page. Because updating every page would be a hassle, all you have to do is add a new object to the links array, and every page with this script linked will automatically update.
-// Links to be made navigation buttons
-// To add a button, just add another object to the array
-var links = [
-  {
-    name: "Home",
-    url: "index.html"
-  },
-  
-  {
-    name: "Keeper",
-    url: "keeper.html"
-  }
-  
-  /*
-  {
-    name: "About Us",
-    url: "about"
-  }
-  */
-];
+    // Created by footykeeper Copyright (c) 2017
+    // View orginal source code: https://github.com/footykeeper/crispy-goggles
+    // View website: http://footykeeper.com
+    // This is navigation.js. This Javascript is designed to fill in the navigation links on every page. Because updating every page would be a hassle, all you have to do is add a new object to the links array, and every page with this script linked will automatically update.
+    // Links to be made navigation buttons
+    // To add a button, just add another object to the array
+    var links = [
+      {
+        name: "Home",
+        url: "index.html"
+      },
 
-var link = null;
+      {
+        name: "Keeper",
+        url: "keeper.html"
+      }
 
-function createLinks () {
-  // Set link to an empty string
-  link = "";
-  
-  // Add navigation buttons
-  for (i = 0; i < links.length; i++) {
-    link += "<td class='nav-button'><a href='" + links[i].url + "' class='plain-link'>" + links[i].name + "</a></td><td></td>";
-  }
-  
-  // Fill in the table with the links
-  document.getElementById("targetRow").innerHTML = link;
-}
+
+      //{
+        //name: "About Us",
+        //url: "about"
+      //}
+
+    ];
+
+    var link = null;
+
+    function createLinks () {
+      // Set link to an empty string
+      link = "";
+
+      // Add navigation buttons
+      for (i = 0; i < links.length; i++) {
+        link += "<td class='nav-button'><a href='" + links[i].url + "' class='plain-link'>" + links[i].name + "</a></td><td class='null'></td>";
+      }
+      // Fill in the table with the links
+      document.getElementById("targetRow").innerHTML = link;
+    }
