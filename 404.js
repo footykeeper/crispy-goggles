07dc42c6adce14c794f4060159814fa741265d58
soccerJoshNumberNine <blue@smittycraft.com>
2017-07-13 09:15:17 -0700

    Update 404.js

--- 404.js
+++ 404.js
@@ -3,12 +3,12 @@ var image = null;
 var credit = null;
 var gifs = [
   {
-    url: "<br/><img src='http://data.whicdn.com/images/140382223/original.gif'/>",
+    url: "<br/><br/><img src='http://data.whicdn.com/images/140382223/original.gif'/>",
     credit: "<br/><p>From We Heart It</p>"
   },
   
   {
-    url: "<br/><img src='https://media.tenor.com/images/1fdc8b1ef9f184bbe9c28d63f68e39bb/tenor.gif'/>",
+    url: "<br/><br/><img src='https://media.tenor.com/images/1fdc8b1ef9f184bbe9c28d63f68e39bb/tenor.gif'/>",
     credit: "<br/><p>tenor</p>"
   }
 ];
