039ada15916200a069bae15ef5c89306e0d8ee40
soccerJoshNumberNine <blue@smittycraft.com>
2017-08-07 08:33:36 -0700

    Delete library.js

--- library.js
+++
@@ -1,8 +0,0 @@
-// Extremely basic, jQuery document ready loads the images and updates a gif count object in the document.
-$(document).ready(function () {
-  $('#gifCount').html(gifs.length);
-  for (i = 0; i < gifs.length; i++) {
-    $('#gallery').append(gifs[i].url);
-    $('#gallery').append(gifs[i].credit);
-  }
-});
