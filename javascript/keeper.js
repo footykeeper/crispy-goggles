00dedc4d1ff0ae59b8928cbcda95394c861cd96c
soccerJoshNumberNine <blue@smittycraft.com>
2017-08-07 10:22:24 -0700

    Update keeper.js

--- javascript/keeper.js
+++ javascript/keeper.js
@@ -26,3 +26,23 @@ function prepForRosterInput () {
   document.getElementById('homeScreen').style.display = 'none';
   document.getElementById('rosterInput').style.display = 'block';
 }
+
+function waitForStart () {
+  for (i = 0; i < document.getElementsByClassName('pName').length; i++) {
+    if (document.getElementsByClassName('pName')[i].value === '' || document.getElementsByClassName('pNumber')[i].value === '') {
+      alert('Please fill out name and number for each player.');
+      return;
+    } else if (!Number.isInteger(parseInt(document.getElementsByClassName('pNumber')[i].value, 10)) || document.getElementsByClassName('pNumber')[i].value < 1 || document.getElementsByClassName('pNumber')[i].value > 99) {
+      alert('Player number must be an integer between 1 and 99.');
+      return;
+    }
+  }
+  
+  for (i = 0; i < playerCount; i++) {
+    roster.push(document.getElementsByClassName('pName')[i].value);
+    playerNumbers.push(document.getElementsByClassName('pNumber')[i].value);
+  }
+  
+  document.getElementById('rosterInput').style.display = 'none';
+  document.getElementById('stageOne').style.display = 'block';
+}
